107176b763d0b68dda520bc960bb7cae9a91a4e7
Ethan Printz <ethanprintz@gmail.com>
2016-03-04 13:07:55 -0800

    Added more code to files

--- journal.js
+++ journal.js
@@ -1,3 +1,8 @@
 $("document").ready(function(){
-   alert("HI!"); 
+    $(".page").each(function(){
+        var pageWidth = $(this).css("width");
+        alert(pageWidth);
+        var pageWidth = praseInt(pageWidth.replace("px",""))*(8.5/11);
+        alert(pageWidth);
+    });
 });
