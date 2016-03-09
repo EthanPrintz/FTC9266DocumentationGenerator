5c84f626466801abbcfad1b7975943b09ffe05f5
Ethan Printz <ethanprintz@gmail.com>
2016-03-09 15:46:10 -0800

    Added CSS Variables, font awesome

--- journal.js
+++ journal.js
@@ -1,8 +1,3 @@
 $("document").ready(function(){
-    $(".page").each(function(){
-        var pageWidth = $(this).css("width");
-        alert(pageWidth);
-        var pageWidth = praseInt(pageWidth.replace("px",""))*(8.5/11);
-        alert(pageWidth);
-    });
+    
 });
